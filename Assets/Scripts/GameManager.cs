0c07a04a772081fb315ddbe3c4feee3b32d09e06
mijkolsmith <mijkolsmith@live.com>
2021-01-29 20:46:45 +0100

    changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	internal static GameManager instance = null;
+
+	private void Awake()
+    {
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	private void Update()
+	{
+		
+	}
+}
