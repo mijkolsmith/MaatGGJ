021d31f5edbc3e0354a8d34ea60921bcc0b07db6
mijkolsmith <mijkolsmith@live.com>
2021-01-31 00:04:29 +0100

    tried to fix portals as best as i could

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : PortalTraveller
 {
 	[SerializeField] public CharacterController charCtrl;
 	[SerializeField] private Transform groundCheck;
