7cf59d9062cf6c7169024ec01c428dfa529fde4f
mijkolsmith <mijkolsmith@live.com>
2021-01-31 12:49:09 +0100

    level1 works

--- Assets/Scripts/CollectResource.cs
+++ Assets/Scripts/CollectResource.cs
@@ -8,8 +8,9 @@ public class CollectResource : MonoBehaviour
 	{
 		if (other.gameObject == Player.Instance.gameObject)
 		{
+			
 			AudioManager.Instance.pickupSFX.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
-			AudioManager.Instance.PlaySFX(AudioType.SFX_PICKUP);
+			AudioManager.Instance.PlayAudio(AudioType.SFX_PICKUP);
 			GameManager.Instance.rm.AddResource(1);
 			Destroy(gameObject);
 		}
